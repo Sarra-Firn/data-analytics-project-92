da590a15fd97ce5e2f6064ebe0cbbe40b4640d76
Sarra-Firn <sarrafirn@gmail.com>
2024-11-27 22:12:50 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -19,7 +19,6 @@ limit 10;
 
 /*Отчёт с продавцами, чья выручка ниже средней выручки всех продавцов:*/
 
-with tab as (
 select CONCAT(e.first_name, ' ', e.last_name) as seller, 
 		FLOOR(AVG(p.price*s.quantity)) as average_income
 		FROM employees as e
@@ -27,16 +26,12 @@ left join sales as s
 on  e.employee_id=s.sales_person_id
 inner join products as p
 on s.product_id = p.product_id
-group by CONCAT(e.first_name, ' ', e.last_name))
-select tab.seller,
-	 	tab.average_income
-from tab
-group by tab.seller, tab.average_income
-having tab.average_income < (select AVG(p.price*s.quantity) as avg
+group by CONCAT(e.first_name, ' ', e.last_name)
+having FLOOR(AVG(p.price*s.quantity)) < (select AVG(p.price*s.quantity) as avg
 							FROM products as p
 							left join sales as s
 							on p.product_id=s.product_id)
-order by tab.average_income;
+order by average_income;
 
 
 /*Отчёт с данными по выручке по каждому продавцу и дню недели:*/
